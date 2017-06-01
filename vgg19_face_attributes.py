057b5b31e3c50b9f5d5aa08d46d9f9f48c30f16c
David Olarte <hernan940730@gmail.com>
2017-06-01 18:27:07 -0500

    keras VGG19 neural network added

--- vgg19_face_attributes.py
+++ vgg19_face_attributes.py
@@ -6,37 +6,58 @@ import sys, getopt
 import numpy as np
     
 image_path = "cat.jpg"
-args_array = ["image_path="]
+weights_path = None
+train_model = False
 
-def configure_args(argv):
+args_array = ["image_path=", "train_model="]
+
+def load_args(argv):
+    '''
+    Load the arguments given by the user
+    '''
     global image_path
     try:
-        opts, args = getopt.getopt(argv, "i:", args_array)
+        opts, args = getopt.getopt(argv, "i:t:", args_array)
     except getopt.GetoptError:
         print ('Invalid option', argv)
         sys.exit(2)
     for opt, arg in opts:
         if opt in ("-i", "--image_path"):
             image_path = arg
+        elif opt in ("-t", "--train_model"):
+            if arg in (True, False):
+                train_model = arg
+            else:
+                print ('Invalid option', opt, arg)
+                sys.exit(2)
         else:
             print ('Invalid option', argv)
             sys.exit(2)
-            
-if __name__ == "__main__":
-    configure_args(sys.argv[1:])
+
+def load_model(weights_path):
+    '''
+    Load the model for the Neural Network
+    '''
+    model = VGG19(include_top=False, weights=None, input_tensor=None, input_shape=None)
     
-    print ("Loading model...")
-    model = VGG19(weights='imagenet')
-    print ("Model loaded.")
-    
-    print ("Loading image...")
+    return model
+
+def predict():
     img = image.load_img(image_path, target_size=(224, 224))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
-    print ("Image loaded.")
-    print ("Predicting...")
     preds = model.predict(x)
-    # decode the results into a list of tuples (class, description, probability)
-    # (one such list for each sample in the batch)
-    print('Predicted:', decode_predictions(preds, top=3)[0])
+    return preds
+
+if __name__ == "__main__":
+    
+    load_args(sys.argv[1:])
+    
+    print ("Loading model...")
+    model = load_model()
+    print ("Model loaded.")
+    
+    print ("Predicting...")
+    preds = predict()
+    print ('Predicted:', decode_predictions(preds, top=3)[0])
